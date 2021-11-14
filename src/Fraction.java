28ff981435e42b9fc028983a7093a5d5359e7ff4
Abahri El Mahdi <elmahdi.abahri@ens.uvsq.com>
2021-11-14 22:38:24 +0100

    First Commit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Fraction {
+public class Fraction {
+    private float a, b;
+
+    public Fraction() {
+        a = 1;
+        b = 1;
+    }
+
+    public Fraction(float a) {
+        this.a = a;
+        this.b = 1;
+    }
+
+    public Fraction(float a, float b) {
+        this.a = a;
+        if (b != 0)
+            this.b = b;
+        else
+            this.b = 1;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public float getA() {
+        return this.a;
+    }
+
+    public float getB() {
+        return this.b;
+    }
+
+    public void Mult(Fraction f) {
+        this.a *= f.getA();
+        this.b *= f.getB();
+    }
+
+    public void Add(Fraction f) {
+        this.a = this.a * f.getB() + this.b * f.getA();
+        this.b = this.a * f.getB();
+    }
 }
